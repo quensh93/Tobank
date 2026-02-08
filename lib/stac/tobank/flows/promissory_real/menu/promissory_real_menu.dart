fc1e886f8354a5721894bfa8b1a3049355c8d003
ali sinaee <alisinaiasl@gmail.com>
2026-02-08 11:11:19 +0330

    merged with mjp

--- lib/stac/tobank/flows/promissory_real/menu/promissory_real_menu.dart
+++ lib/stac/tobank/flows/promissory_real/menu/promissory_real_menu.dart
@@ -1,5 +1,4 @@
 import 'package:stac_core/stac_core.dart';
-import '../../../../../core/stac/builders/stac_common_builders.dart';
 
 /// Promissory Real Flow - Debug Menu
 ///
@@ -90,7 +89,7 @@ StacWidget promissoryRealDebugMenu() {
             ),
           ),
           StacSizedBox(height: 16),
-          // 3. Load from API
+          // 4. Static Login
           StacOutlinedButton(
             onPressed: StacRawJsonAction({
               'actionType': 'navigate',
@@ -98,7 +97,9 @@ StacWidget promissoryRealDebugMenu() {
               'navigationStyle': 'pushReplacement',
             }),
             style: StacButtonStyle(
-              padding: StacEdgeInsets.symmetric(vertical: 16),
+              backgroundColor: '#FF5722',
+              foregroundColor: '#FFFFFF',
+              padding: StacEdgeInsets.symmetric(horizontal: 32, vertical: 16),
             ),
             child: StacText(
               data: '{{appStrings.promissory.loadFromJsonApi}}',
@@ -121,28 +122,6 @@ StacWidget promissoryRealDebugMenu() {
           ),
           StacSizedBox(height: 16),
 
-          // 4. Static Login
-          StacOutlinedButton(
-            onPressed: StacRawJsonAction({
-              'actionType': 'navigate',
-              'widgetType': 'promissory_real_loader',
-              'navigationStyle': 'pushReplacement',
-            }),
-            style: StacButtonStyle(
-              backgroundColor: '#FF5722',
-              foregroundColor: '#FFFFFF',
-              padding: StacEdgeInsets.symmetric(horizontal: 32, vertical: 16),
-            ),
-            child: StacText(
-              data: 'API JSON',
-              style: StacTextStyle(
-                fontSize: 16,
-                fontWeight: StacFontWeight.w600,
-              ),
-            ),
-          ),
-
-          const StacSizedBox(height: 16),
           StacFilledButton(
             onPressed: StacRawJsonAction({
               'actionType': 'promissory_real_login',
